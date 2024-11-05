28de8a671e21ea466675efb43e3afd399c43d3f3
aetaric <565454+aetaric@users.noreply.github.com>
2024-11-04 20:35:53 -0700

    Fix for resizing

--- Scenes/WindowSpawn.gd
+++ Scenes/WindowSpawn.gd
@@ -1,10 +1,10 @@
 # TestScene.gd
 extends Node2D
 
-var window_scene = preload("res://WindowTest.tscn")  # Adjust path as needed
+var window_scene := preload("res://Scenes/WindowTest.tscn")  # Adjust path as needed
 
 func _on_pond_pressed():  # Fixed function name formatting
-	var new_window = window_scene.instantiate()
+	var new_window := window_scene.instantiate()
 	add_child(new_window)
 	# Spawn window at random position
 	new_window.position = Vector2(

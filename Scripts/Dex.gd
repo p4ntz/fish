e5d53fea5f5c18d81ffbba50ed522a85d5519dc4
aetaric <565454+aetaric@users.noreply.github.com>
2024-11-05 21:55:21 -0700

    fix dex

--- Scripts/Dex.gd
+++ Scripts/Dex.gd
@@ -11,7 +11,7 @@ func init_data() -> Dex:
 	var json_as_text := FileAccess.get_file_as_string(file)
 	var fishNames: Dictionary = JSON.parse_string(json_as_text)
 	var locations: Array = []
-	for index in fishNames.keys():      
+	for index in fishNames.keys():
 		var fish = Fish.new()
 		fish.fish_name = fishNames[index]["fish_name"]
 		fish.catching_difficulty = fishNames[index]["difficulty"]
@@ -21,6 +21,7 @@ func init_data() -> Dex:
 		fish.locations = fishNames[index]["locations"]
 		fish.seasons = fishNames[index]["seasons"]
 		fish.time_of_day = fishNames[index]["time_of_day"]
+		fish.sprite_path = fishNames[index]["sprite_path"]
 		for location in fishNames[index]["locations"]:
 			if not location in locations:
 				locations.append(fishNames[index]["locations"])
@@ -34,10 +35,10 @@ func init_data() -> Dex:
 func random_fish() -> Fish:
 	var selected: int = randi_range(0,len(FishData.keys()) - 1)
 	var check_fish: Fish = FishData[FishData.keys()[selected]]
-	while not check_fish.can_be_caught_now("evening", "summer", "river"):
+	while not check_fish.can_be_caught_now(Globals.current_time, Globals.current_season, Globals.current_fishing_location):
 		selected = randi_range(0,len(FishData.keys()) - 1)
 		check_fish = FishData[FishData.keys()[selected]]
-		
+
 	tracked_fish = check_fish
 	print(check_fish.fish_name)
 	return tracked_fish
